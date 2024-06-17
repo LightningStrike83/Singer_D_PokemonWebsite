bc5cde2ff91ffb03293e02d141585732d53d0fb3
Dan Singer <d_singer@fanshaweonline.ca>
2024-06-16 22:08:38 -0400

    Header Update
    Updated the header to accomodate more pages

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 const hamMenu = document.querySelector("#hamburger-menu")
+const infoMenu = document.querySelector("#game-info-menu")
+const activityMenu = document.querySelector("#activity-menu")
+const infoButton = document.querySelector("#game-info-button")
+const activityButton = document.querySelector("#activity-button")
 
 function openHamMenu() {
     var navMenu = document.querySelector("#nav-menu")
@@ -12,4 +16,37 @@ function openHamMenu() {
     }
 }
 
+function openInfoMenu() {
+    if (infoMenu.style.display === "none") {
+        infoMenu.style.display = "flex"
+    } else (
+        infoMenu.style.display = "none"
+    )
+
+    if (activityMenu.style.display = "flex") {
+        activityMenu.style.display = "none"
+    }
+}
+
+function openActivityMenu() {
+    if (activityMenu.style.display === "none") {
+        activityMenu.style.display = "flex"
+    } else (
+        activityMenu.style.display = "none"
+    )
+
+    if (infoMenu.style.display = "flex") {
+        infoMenu.style.display = "none"
+    }
+}
+
+function setDefault() {
+    activityMenu.style.display = "none"
+    infoMenu.style.display = "none"
+}
+
+setDefault()
+
 hamMenu.addEventListener("click", openHamMenu)
+infoButton.addEventListener("click", openInfoMenu)
+activityButton.addEventListener("click", openActivityMenu)

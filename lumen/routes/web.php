81357ecdda62add074f5e544996b1a8397a148d6
Dan Singer <d_singer@fanshaweonline.ca>
2024-11-15 01:00:07 -0500

    Then We Fight Page
    -Added page for Then We Fight prompts
    -Added functionality from Lumen
    -Styling for the Then We Fight Page
    -Added links to the new page on all pages
    -Updated titles

--- lumen/routes/web.php
+++ lumen/routes/web.php
@@ -17,6 +17,8 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
+$router->get("/prompts/all", "TWFPromptController@getAll");
+// $router->get("/prompts/submit", "TWFSuggestionController@save");
 $router->get("/updates", "SiteUpdateController@getThree");
 $router->get("/lines", "LineController@getAll");
 $router->get("/lines/{id}", "LineController@getOne");
